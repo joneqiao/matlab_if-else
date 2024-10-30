9ceee5e7fd9371dbd989a0d7970706b24a9debaa
jone <763302501@qq.com>
2024-10-30 21:16:27 +0800

    switch first commit

---
+++ switch.m
@@ -0,0 +1,18 @@
+%定义一个字符数组 daystring
+
+switch daystring
+    
+    case 'Monday'
+        disp('Start of the work week')
+    case  'Tuesday'
+        disp('day 2')
+    case 'Wednesday'
+        disp('day 3')
+    case 'Thursday'
+        disp('day 4')
+    case 'Friday'
+        disp('day 5')
+    case 'Saturday'
+        disp('End of the work week')
+    case 'Sunday'
+        disp('Weekend')
